7e739e8c6d195bf5f2f3406b0f3835a109c6695d
Joe Maples <joe@frap129.org>
2016-07-10 17:26:44 +0000

    cpufreq: Add Chill cpu gov
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Planet-X <planetx.dev@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -566,6 +566,9 @@ extern struct cpufreq_governor cpufreq_gov_bioshock;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CULTIVATION)
 extern struct cpufreq_governor cpufreq_gov_cultivation;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cultivation)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CHILL)
+extern struct cpufreq_governor cpufreq_gov_chill;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_chill)
 #endif
 
 /*********************************************************************
