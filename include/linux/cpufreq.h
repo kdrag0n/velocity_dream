a26a467f336c449ee09821fea924c3888c34dcbe
Planet-X <planetx.dev@gmail.com>
2017-11-24 17:24:40 +0100

    cpufreq: Add zzmoove governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -587,6 +587,9 @@ extern struct cpufreq_governor cpufreq_gov_relaxed;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
 extern struct cpufreq_governor cpufreq_gov_yankactive;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_yankactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_zzmoove)
 #endif
 
 /*********************************************************************
