7c3f91e939e6a6a42aeb4d01e63729a06c40409b
djb77 <dwayne.bakewell@gmail.com>
2017-11-29 11:29:49 +1100

    LINUX: 4.4.15 Kernel Update
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- drivers/usb/dwc3/dwc3-exynos.c
+++ drivers/usb/dwc3/dwc3-exynos.c
@@ -505,12 +505,6 @@ static int dwc3_exynos_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, exynos);
 
-	ret = dwc3_exynos_register_phys(exynos);
-	if (ret) {
-		dev_err(dev, "couldn't register PHYs\n");
-		return ret;
-	}
-
 	exynos->dev = dev;
 
 	exynos->drv_data = dwc3_exynos_get_driver_data(pdev);
@@ -566,16 +560,22 @@ static int dwc3_exynos_probe(struct platform_device *pdev)
 		}
 	}
 
+	ret = dwc3_exynos_register_phys(exynos);
+	if (ret) {
+		dev_err(dev, "couldn't register PHYs\n");
+		goto err4;
+	}
+
 	if (node) {
 		ret = of_platform_populate(node, NULL, NULL, dev);
 		if (ret) {
 			dev_err(dev, "failed to add dwc3 core\n");
-			goto err4;
+			goto err5;
 		}
 	} else {
 		dev_err(dev, "no device node, failed to add dwc3 core\n");
 		ret = -ENODEV;
-		goto err4;
+		goto err5;
 	}
 
 	lockup_noti = false;
@@ -584,6 +584,9 @@ static int dwc3_exynos_probe(struct platform_device *pdev)
 	dev_info(dev, "%s: -\n", __func__);
 	return 0;
 
+err5:
+	platform_device_unregister(exynos->usb2_phy);
+	platform_device_unregister(exynos->usb3_phy);
 err4:
 	if (exynos->vdd10)
 		regulator_disable(exynos->vdd10);
