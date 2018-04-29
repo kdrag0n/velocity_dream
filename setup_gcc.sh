7436f8799e21479a1cd933baa16e6277f276b31f
kdrag0n <dragon@khronodragon.com>
2018-04-29 15:39:41 -0700

    build: switch setup_gcc to cortex-a53 gcc 6
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- setup_gcc.sh
+++ setup_gcc.sh
@@ -1,6 +1,7 @@
 #export CROSS_COMPILE=$HOME/code/android/google49/bin/aarch64-linux-android-
 #export CROSS_COMPILE=$HOME/code/android/gclang/clang-4691093/bin/
-export CROSS_COMPILE=$HOME/code/android/linaro731/bin/aarch64-linux-gnu-
+#export CROSS_COMPILE=$HOME/code/android/linaro731/bin/aarch64-linux-gnu-
+export CROSS_COMPILE=$HOME/code/android/cortex53/bin/aarch64-cortex_a53-linux-gnueabi-
 export ARCH=arm64
 export SUBARCH=arm64
 export CFLAGS="-Ofast"
