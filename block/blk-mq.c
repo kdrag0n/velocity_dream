26f0ab57f87784eb0ab01ed417a318283ad99b1c
kdrag0n <dragon@khronodragon.com>
2018-05-13 15:27:10 -0700

    Revert "blk-mq: immediately dispatch big size request"
    This reverts commit 5a1b2b217d05d921a1322b49c8798f8908c094e7.

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -1450,8 +1450,6 @@ static blk_qc_t blk_sq_make_request(struct request_queue *q, struct bio *bio)
 	 */
 	plug = current->plug;
 	if (plug) {
-		struct request *last = NULL;
-
 		blk_mq_bio_to_request(rq, bio);
 
 		/*
@@ -1462,13 +1460,10 @@ static blk_qc_t blk_sq_make_request(struct request_queue *q, struct bio *bio)
 			request_count = 0;
 		if (!request_count)
 			trace_block_plug(q);
-		else
-			last = list_entry_rq(plug->mq_list.prev);
 
 		blk_mq_put_ctx(data.ctx);
 
-		if (request_count >= BLK_MAX_REQUEST_COUNT || (last &&
-		    blk_rq_bytes(last) >= BLK_PLUG_FLUSH_SIZE)) {
+		if (request_count >= BLK_MAX_REQUEST_COUNT) {
 			blk_flush_plug_list(plug, false);
 			trace_block_plug(q);
 		}
