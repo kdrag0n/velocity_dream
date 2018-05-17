7f204bfe3dea3a5fed53068445606baad14a29a7
kdrag0n <dragon@khronodragon.com>
2018-05-16 17:03:53 -0700

    max77865: support unstable power detection
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/battery_v2/max77865_charger.c
+++ drivers/battery_v2/max77865_charger.c
@@ -1477,6 +1477,8 @@ static void max77865_enable_aicl_irq(struct max77865_charger_data *charger)
 	pr_info("%s enabled : %d\n", __func__, charger->irq_aicl_enabled);
 }
 
+bool unstable_power_detection = true;
+
 static void max77865_chgin_isr_work(struct work_struct *work)
 {
 	struct max77865_charger_data *charger = container_of(work,
@@ -1513,7 +1515,7 @@ static void max77865_chgin_isr_work(struct work_struct *work)
 			stable_count++;
 		else
 			stable_count = 0;
-		if (stable_count > 10) {
+		if (stable_count > 10 || !unstable_power_detection) {
 			pr_info("%s: irq(%d), chgin(0x%x), chg_dtls(0x%x) prev 0x%x\n",
 					__func__, charger->irq_chgin,
 					chgin_dtls, chg_dtls, prev_chgin_dtls);
