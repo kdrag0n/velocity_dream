99e64ed215359de813e8f28f457fa635e97355d3
kdrag0n <dragon@khronodragon.com>
2018-05-18 22:02:50 -0700

    setup: clean up scripts
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- setup.sh
+++ setup.sh
@@ -1,18 +1,7 @@
 # Toolchain paths
 
-# Path to the root of the clang toolchain
-# Do not add /bin/... or anything.
-tc_clang=$HOME/code/android/dtc/out/7.0
-
-# Whether the given clang toolchain is DragonTC.
-# Controls use of optimizations.
-# Value: true or false
-dragontc=true
-
-# Path to the root of the gcc toolchain.
-# Must be recent, or you may encounter problems.
-# Do not add /bin/... or anything.
-tc_gcc=$HOME/code/android/linaro731
+# Path to the GCC toolchain, including the target prefix.
+tc=$HOME/code/android/linaro641/bin/aarch64-linux-gnu-
 
 # Number of parallel jobs to run
 # This should be set to the number of CPU cores on your system.
@@ -22,28 +11,18 @@ jobs=10
 # Do not edit below this point
 # -----------------------------
 
-export CLANG_PREBUILT_BIN=$tc_clang/bin/
-export CROSS_COMPILE=aarch64-linux-gnu-
-export CC=clang
-export CLANG_TRIPLE=aarch64-linux-gnu-
-export GCC_TOOLCHAIN=$tc_gcc
+export CROSS_COMPILE=$tc
 export ARCH=arm64
-export LD_LIBRARY_PATH=$tc_clang/lib64:$LD_LIBRARY_PATH
-export PATH=$CLANG_PREBUILT_BIN:$GCC_TOOLCHAIN/bin:$PATH
-export REAL_COMPILER=clang
 export SUBARCH=arm64
 export KBUILD_BUILD_USER=velocity
 export KBUILD_BUILD_HOST=kernel
-export TOOL_CHAIN_PATH=$tc_gcc/bin/aarch64-linux-gnu-
-export CLANG_TCHAIN=$CLANG_PREBUILT_BIN/clang
-export CLANG_VERSION="$(${CLANG_TCHAIN} --version|head -n1|cut -d'(' -f1,4|sed -e 's/^\s*//' -e 's/\s*$//')"
-export REAL_COMPILER=clang
-export DRAGONTC=$dragontc
 
 export CFLAGS=""
 export CXXFLAGS=""
 export LDFLAGS=""
 
-alias make="make CC=$CLANG_TCHAIN CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=$TOOL_CHAIN_PATH KBUILD_COMPILER_STRING=\"${CLANG_VERSION}\" KBUILD_BUILD_VERSION=1 HOSTCC=$CLANG_TCHAIN"
+cc_ver="$(${tc}gcc --version|head -n1|cut -d'(' -f2|tr -d ')')"
+
+alias make="make KBUILD_COMPILER_STRING=\"${cc_ver}\" KBUILD_BUILD_VERSION=1"
 
 source helpers.sh
