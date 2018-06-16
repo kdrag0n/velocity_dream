5ed898c27b0229284874c136c9455bbca39317bf
kdrag0n <dragon@khronodragon.com>
2018-06-16 14:53:43 -0700

    init: increase ZRAM size, add deep sleep fix
    1000 MB of ZRAM doesn't really seem to be enough for bloated Samsung
    ROMs, so increase it to 1400 MB.
    
    Add the SCSI drive deep sleep fix by Chainfire.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- flasher/ramdisk-patch/init.spectrum.sh
+++ flasher/ramdisk-patch/init.spectrum.sh
@@ -7,7 +7,19 @@ if [ ! -f /data/property/persist.spectrum.profile ]; then
 	setprop persist.spectrum.profile 0
 fi
 
-# Additional tweaks
-stop secure_storage # fix battery drain
+# VELOCITY KERNEL
+
+# fix battery drain
+stop secure_storage
+
+# activate zswap
 mkswap /dev/block/zram0
 swapon /dev/block/zram0 # activate zswap
+
+# fix deep sleep (thanks to Chainfire)
+for i in $(ls /sys/class/scsi_disk/); do
+	cat /sys/class/scsi_disk/$i/write_protect 2>/dev/null | grep 1 >/dev/null
+	if [ $? -eq 0 ]; then
+		echo 'temporary none' > /sys/class/scsi_disk/$i/cache_type
+	fi
+done
