a5ab44c5183e7352edc6c42179466cb68fd99132
kdrag0n <dragon@khronodragon.com>
2018-06-29 21:19:11 -0700

    init: activate zram early instead of after boot
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- flasher/ramdisk-patch/init.early.sh
+++ flasher/ramdisk-patch/init.early.sh
@@ -4,3 +4,9 @@ enforce=0
 [ $enforce = 0 ] && chmod 440 /sys/fs/selinux/policy
 echo $enforce > /sys/fs/selinux/enforce
 [ $enforce = 0 ] && chmod 640 /sys/fs/selinux/enforce
+
+# activate zram
+if [ "$(cat /proc/swaps|wc -l)" = 1 ]; then
+    mkswap /dev/block/zram0
+    swapon /dev/block/zram0
+fi
