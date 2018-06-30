f8dee9120c3ff2a8cf1eb41240ebe48ae1fb613e
kdrag0n <dragon@khronodragon.com>
2018-06-29 20:13:59 -0700

    flasher: fix Settings displaying Permissive all the time
    Settings displayed Permissive even when the real status was Enforcing
    because it was unable to read the policy and enforcing status files.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- flasher/ramdisk-patch/init.early.sh
+++ flasher/ramdisk-patch/init.early.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 enforce=0
 
-chmod 440 /sys/fs/selinux/policy
+[ $enforce = 0 ] && chmod 440 /sys/fs/selinux/policy
 echo $enforce > /sys/fs/selinux/enforce
-chmod 640 /sys/fs/selinux/enforce
+[ $enforce = 0 ] && chmod 640 /sys/fs/selinux/enforce
