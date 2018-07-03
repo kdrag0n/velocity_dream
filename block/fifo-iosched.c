ef568316bc0ba9649c99c765fd054b318427f29f
kdrag0n <dragon@khronodragon.com>
2018-07-03 14:52:10 -0700

    Revert "iosched: fix fifo"
    This reverts commit af76ddb20270b75f8b6d9a7456ca708b3500ad94.

--- block/fifo-iosched.c
+++ block/fifo-iosched.c
@@ -31,15 +31,28 @@ static void fifo_add_request(struct request_queue *q, struct request *req)
 	list_add_tail(&req->queuelist, &fifo_d->queue);
 }
 
-static void *fifo_init_queue(struct request_queue *q)
+static int fifo_init_queue(struct request_queue *q, struct elevator_type *e)
 {
 	struct fifo_data *fifo_d;
+	struct elevator_queue *eq;
+
+	eq = elevator_alloc(q, e);
+	if (!eq)
+		return -ENOMEM;
 
 	fifo_d = kmalloc_node(sizeof(*fifo_d), GFP_KERNEL, q->node);
-	if (!fifo_d)
-		return NULL;
+	if (!fifo_d) {
+		kobject_put(&eq->kobj);
+		return -ENOMEM;
+	}
+	eq->elevator_data = fifo_d;
+
 	INIT_LIST_HEAD(&fifo_d->queue);
-	return fifo_d;
+
+	spin_lock_irq(q->queue_lock);
+	q->elevator = eq;
+	spin_unlock_irq(q->queue_lock);
+	return 0;
 }
 
 static void fifo_exit_queue(struct elevator_queue *e)
@@ -70,8 +83,7 @@ static struct elevator_type elevator_fifo = {
 
 static int __init fifo_init(void)
 {
-	elv_register(&elevator_fifo);
-	return 0;
+	return elv_register(&elevator_fifo);
 }
 
 static void __exit fifo_exit(void)
