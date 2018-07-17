70eed448570c31461b72b31412286db1f5f4a2ef
kdrag0n <dragon@khronodragon.com>
2018-07-16 19:55:02 -0700

    setup_clang: update script
    Changes:
     - Use latest AOSP Clang 7.0.2 instead of DragonTC 7.0
     - Use 8 parallel jobs instead of 10
     - Remove useless SUBARCH variable
     - Update clang version string filter for AOSP clang
     - Unalias some common alias that may interfere with build
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- setup_clang.sh
+++ setup_clang.sh
@@ -2,7 +2,7 @@
 
 # Path to the root of the clang toolchain
 # Do not add /bin/... or anything.
-tc_clang=$HOME/code/android/dtc/out/7.0
+tc_clang=$HOME/code/android/gclang/clang-r328903
 
 # Whether the given clang toolchain is DragonTC.
 # Controls use of optimizations.
@@ -17,7 +17,7 @@ tc_gcc=$HOME/code/android/linaro641
 # Number of parallel jobs to run
 # This should be set to the number of CPU cores on your system.
 # Do not remove, set to 1 for no parallelism.
-jobs=10
+jobs=8
 
 # Do not edit below this point
 # -----------------------------
@@ -31,12 +31,12 @@ export ARCH=arm64
 export LD_LIBRARY_PATH=$tc_clang/lib64:$LD_LIBRARY_PATH
 export PATH=$CLANG_PREBUILT_BIN:$GCC_TOOLCHAIN/bin:$PATH
 export REAL_COMPILER=clang
-export SUBARCH=arm64
 export KBUILD_BUILD_USER=velocity
 export KBUILD_BUILD_HOST=kernel
 export TOOL_CHAIN_PATH=$tc_gcc/bin/aarch64-linux-gnu-
 export CLANG_TCHAIN=$CLANG_PREBUILT_BIN/clang
-export CLANG_VERSION="$(${CLANG_TCHAIN} --version|head -n1|cut -d'(' -f1,4|sed -e 's/^\s*//' -e 's/\s*$//')"
+#export CLANG_VERSION="$(${CLANG_TCHAIN} --version|head -n1|cut -d'(' -f1,4|sed -e 's/[[:space:]]*$//'|sed 's/\(.+\)//')"
+export CLANG_VERSION="$(${CLANG_TCHAIN} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 export REAL_COMPILER=clang
 export DRAGONTC=$dragontc
 
@@ -44,6 +44,10 @@ export CFLAGS=""
 export CXXFLAGS=""
 export LDFLAGS=""
 
+unalias cat > /dev/null 2>&1
+unalias zip > /dev/null 2>&1
+
 MAKEFLAGS=(CC=$CLANG_TCHAIN CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=$TOOL_CHAIN_PATH "KBUILD_COMPILER_STRING=${CLANG_VERSION}" HOSTCC=$CLANG_TCHAIN)
 
+# helpers
 source helpers.sh
