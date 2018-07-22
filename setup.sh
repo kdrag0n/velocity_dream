2b74eef24dbaa8125a54b22b78e5e73dcb9820df
kdrag0n <dragon@khronodragon.com>
2018-07-21 21:02:31 -0700

    setup: primarily use clang
    GCC is now an alternate script.

--- setup.sh
+++ setup.sh
@@ -1,28 +1,47 @@
 # Toolchain paths
 
-# Path to the GCC toolchain, including the target prefix.
-tc=$HOME/code/android/bootlin730/bin/aarch64-buildroot-linux-gnu-
+# Path to the root of the clang toolchain
+# Do not add /bin/... or anything.
+tc_clang=$HOME/code/android/gclang/clang-r328903
+
+# Path to the root of the gcc toolchain.
+# Must be recent, or you may encounter problems.
+# Do not add /bin/... or anything.
+tc_gcc=$HOME/code/android/8tc
 
 # Number of parallel jobs to run
 # This should be set to the number of CPU cores on your system.
 # Do not remove, set to 1 for no parallelism.
-jobs=10
+jobs=8
 
 # Do not edit below this point
 # -----------------------------
 
-export CROSS_COMPILE=$tc
+export CLANG_PREBUILT_BIN=$tc_clang/bin/
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CC=clang
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export GCC_TOOLCHAIN=$tc_gcc
 export ARCH=arm64
-export SUBARCH=arm64
+export LD_LIBRARY_PATH=$tc_clang/lib64:$LD_LIBRARY_PATH
+export PATH=$CLANG_PREBUILT_BIN:$GCC_TOOLCHAIN/bin:$PATH
+export REAL_COMPILER=clang
 export KBUILD_BUILD_USER=velocity
 export KBUILD_BUILD_HOST=kernel
+export TOOL_CHAIN_PATH=$tc_gcc/bin/aarch64-linux-gnu-
+export CLANG_TCHAIN=$CLANG_PREBUILT_BIN/clang
+#export CLANG_VERSION="$(${CLANG_TCHAIN} --version|head -n1|cut -d'(' -f1,4|sed -e 's/[[:space:]]*$//'|sed 's/\(.+\)//')"
+export CLANG_VERSION="$(${CLANG_TCHAIN} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+export REAL_COMPILER=clang
 
 export CFLAGS=""
 export CXXFLAGS=""
 export LDFLAGS=""
 
-cc_ver="$(${tc}gcc --version|head -n1|cut -d'(' -f2|tr -d ')'|awk '{$5=""; print $0}'|sed -e 's/[[:space:]]*$//')"
+unalias cat > /dev/null 2>&1
+unalias zip > /dev/null 2>&1
 
-MAKEFLAGS=("KBUILD_COMPILER_STRING=${cc_ver}")
+MAKEFLAGS=(CC=$CLANG_TCHAIN CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=$TOOL_CHAIN_PATH "KBUILD_COMPILER_STRING=${CLANG_VERSION}" HOSTCC=$CLANG_TCHAIN)
 
+# helpers
 source helpers.sh
---
+++ setup_gcc.sh
@@ -0,0 +1,28 @@
+# Toolchain paths
+
+# Path to the GCC toolchain, including the target prefix.
+tc=$HOME/code/android/bootlin730/bin/aarch64-buildroot-linux-gnu-
+
+# Number of parallel jobs to run
+# This should be set to the number of CPU cores on your system.
+# Do not remove, set to 1 for no parallelism.
+jobs=10
+
+# Do not edit below this point
+# -----------------------------
+
+export CROSS_COMPILE=$tc
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER=velocity
+export KBUILD_BUILD_HOST=kernel
+
+export CFLAGS=""
+export CXXFLAGS=""
+export LDFLAGS=""
+
+cc_ver="$(${tc}gcc --version|head -n1|cut -d'(' -f2|tr -d ')'|awk '{$5=""; print $0}'|sed -e 's/[[:space:]]*$//')"
+
+MAKEFLAGS=("KBUILD_COMPILER_STRING=${cc_ver}")
+
+source helpers.sh
