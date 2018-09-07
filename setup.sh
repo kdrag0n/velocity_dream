f9c32db7d3d53ca08862741732ed413eecb802ed
kdrag0n <dragon@khronodragon.com>
2018-09-06 21:57:53 -0700

    setup: primarily use GCC again
    Benchmarks prove GCC 8 as the superior compiler performance wise. Refer
    to this commit: 25e6c37f24a309c588be6e356a5aeb85033b306c
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
 # Toolchain paths
 
-# Path to the root of the clang toolchain
-# Do not add /bin/... or anything.
-tc_clang=$HOME/toolchains/clang-8.x
-
-# Path to the root of the gcc toolchain.
-# Must be recent, or you may encounter problems.
-# Do not add /bin/... or anything.
-tc_gcc=/usr
+# Path to the GCC toolchain, including the target prefix.
+tc=/usr/bin/aarch64-linux-gnu-
 
 # Number of parallel jobs to run
 # This should be set to the number of CPU cores on your system.
@@ -17,31 +11,18 @@ jobs=8
 # Do not edit below this point
 # -----------------------------
 
-export CLANG_PREBUILT_BIN=$tc_clang/bin/
-export CROSS_COMPILE=aarch64-linux-gnu-
-export CC=clang
-export CLANG_TRIPLE=aarch64-linux-gnu-
-export GCC_TOOLCHAIN=$tc_gcc
+export CROSS_COMPILE=$tc
 export ARCH=arm64
-export LD_LIBRARY_PATH=$tc_clang/lib64:$LD_LIBRARY_PATH
-export PATH=$CLANG_PREBUILT_BIN:$GCC_TOOLCHAIN/bin:$PATH
-export REAL_COMPILER=clang
+export SUBARCH=arm64
 export KBUILD_BUILD_USER=velocity
 export KBUILD_BUILD_HOST=kernel
-export TOOL_CHAIN_PATH=$tc_gcc/bin/aarch64-linux-gnu-
-export CLANG_TCHAIN=$CLANG_PREBUILT_BIN/clang
-#export CLANG_VERSION="$(${CLANG_TCHAIN} --version|head -n1|cut -d'(' -f1,4|sed -e 's/[[:space:]]*$//'|sed 's/\(.+\)//')"
-export CLANG_VERSION="$(${CLANG_TCHAIN} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
-export REAL_COMPILER=clang
 
 export CFLAGS=""
 export CXXFLAGS=""
 export LDFLAGS=""
 
-unalias cat > /dev/null 2>&1
-unalias zip > /dev/null 2>&1
+cc_ver="$(${tc}gcc --version|head -n1|cut -d'(' -f2|tr -d ')'|awk '{$5=""; print $0}'|sed -e 's/[[:space:]]*$//')"
 
-MAKEFLAGS=(CC=$CLANG_TCHAIN CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=$TOOL_CHAIN_PATH "KBUILD_COMPILER_STRING=${CLANG_VERSION}" HOSTCC=$CLANG_TCHAIN)
+MAKEFLAGS=("KBUILD_COMPILER_STRING=${cc_ver}")
 
-# helpers
 source helpers.sh
