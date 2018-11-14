d88e77df23d055ecc025f6395834f2015534e863
kdrag0n <dragon@khronodragon.com>
2018-11-13 21:44:35 -0800

    helpers: display more objects in osize
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- helpers.sh
+++ helpers.sh
@@ -136,5 +136,5 @@ upsfinish() {
 
 # Get a sorted list of the side of various objects in the kernel
 osize() {
-    find out -type f -name '*.o' ! -name 'built-in.o' ! -name 'vmlinux.o' -exec du -h --apparent-size {} + | sort -r -h | head -n 50
+    find out -type f -name '*.o' ! -name 'built-in.o' ! -name 'vmlinux.o' -exec du -h --apparent-size {} + | sort -r -h | head -n 75
 }
