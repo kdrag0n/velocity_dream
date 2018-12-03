1f71c6d2f072c0771215cc22c726cf068c9071fb
kdrag0n <dragon@khronodragon.com>
2018-12-02 20:23:58 -0800

    helpers: use absolute path to kernel root directory
    This makes the helpers work from anywhere. There is no longer a need to
    execute all of the in the root of the kernel repostitory.
    
    This change also makes the helpers work with spaces in the path by
    quoting every usage of the new "kroot" variable.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- helpers.sh
+++ helpers.sh
@@ -1,22 +1,29 @@
+# Shared interactive kernel build helpers
+
+# Root of the kernel repository for use in helpers
+kroot="$(dirname "$0")"
+
 _RELEASE=0
 
 mkzip() {
     [ $_RELEASE -eq 0 ] && vprefix=test
     [ $_RELEASE -eq 1 ] && vprefix=v
 
-    cp -f arch/arm64/boot/dtb{,2}.img flasher/
-    cp -f arch/arm64/boot/Image flasher/
+    cp -f "$kroot/arch/arm64/boot/dtb.img" "$kroot/flasher/"
+    cp -f "$kroot/arch/arm64/boot/dtb2.img" "$kroot/flasher/"
+    cp -f "$kroot/arch/arm64/boot/Image" "$kroot/flasher/"
 
-    [ $_RELEASE -eq 0 ] && echo "Installing test build $(cat .version)" >| flasher/version
-    [ $_RELEASE -eq 1 ] && echo "Installing version v$(cat .version)" >| flasher/version
-    echo "Built on $(date "+%a %b %d, %Y")" >> flasher/version
+    [ $_RELEASE -eq 0 ] && echo "Installing test build $(cat .version)" >| "$kroot/flasher/version"
+    [ $_RELEASE -eq 1 ] && echo "Installing version v$(cat .version)" >| "$kroot/flasher/version"
+    echo "Built on $(date "+%a %b %d, %Y")" >> "$kroot/flasher/version"
 
     fn="${1:-velocity_kernel.zip}"
-    pushd flasher
-    rm -f "../$fn"
+    rm -f "$fn"
     echo "  ZIP     $fn"
-    zip -r9 "../$fn" . > /dev/null
-    popd
+    oldpwd="$(pwd)"
+    pushd -q "$kroot/flasher"
+    zip -qr9 "$oldpwd/$fn" . -x .gitignore
+    popd -q
 }
 
 build_dtb() {
@@ -27,27 +34,27 @@ build_dtb() {
 rel() {
     _RELEASE=1
 
-    # Swap version files
-    [ ! -f .relversion ] && echo 0 > .relversion
-    mv .version .devversion && \
-    mv .relversion .version
+    # Swap out version files
+    [ ! -f "$kroot/.relversion" ] && echo 0 > "$kroot/.relversion"
+    mv "$kroot/.version" "$kroot/.devversion" && \
+    mv "$kroot/.relversion" "$kroot/.version"
 
     # Compile kernel
     make "${MAKEFLAGS[@]}" -j$jobs $@
 
-    # Revert version files
-    mv .version .relversion && \
-    mv .devversion .version
+    # Pack zip
+    mkdir -p "$kroot/builds"
+    mkzip "builds/VelocityKernel-s8-v$(cat "$kroot/.version").zip"
 
-    # Pack release zip
-    mkdir -p builds
-    mkzip "releases/VelocityKernel-s8-r$(cat .relversion).zip"
+    # Revert version
+    mv "$kroot/.version" "$kroot/.relversion" && \
+    mv "$kroot/.devversion" "$kroot/.version"
 
     _RELEASE=0
 }
 
 zerover() {
-    echo 0 >| .version
+    echo 0 >| "$kroot/.version"
 }
 
 cleanbuild() {
@@ -63,10 +70,11 @@ dbuild() {
 }
 
 dzip() {
-    mkdir -p builds
-    mkzip "builds/VelocityKernel-s8-test$(cat .version).zip"
+    mkdir -p "$kroot/builds"
+    mkzip "builds/VelocityKernel-s8-test$(cat "$kroot/.version").zip"
 }
 
+# Flash the latest kernel zip on the connected device via ADB
 ktest() {
     adb wait-for-any
     adb shell ls '/init.recovery*' > /dev/null 2>&1
@@ -76,14 +84,15 @@ ktest() {
 
     fn="${1:-velocity_kernel.zip}"
     adb wait-for-usb-recovery
-    while [ ! -f $fn ]; do
+    while [ ! -f "$fn" ]; do
         sleep 0.05
     done
-    adb push $fn /tmp/kernel.zip && \
+    adb push "$fn" /tmp/kernel.zip && \
     adb shell "twrp install /tmp/kernel.zip && reboot"
-    [ "$fn" = ".vtest.zip" ] && rm -f $fn
+    [ "$fn" = ".vtest.zip" ] && rm -f "$fn"
 }
 
+# Incremementally build the kernel, then flash it on the connected device via ADB
 inc() {
     make "${MAKEFLAGS[@]}" -j$jobs $@ && \
     {
@@ -95,11 +104,12 @@ inc() {
 }
 
 dc() {
-    diff arch/arm64/configs/velocity_defconfig .config
+    diff arch/arm64/configs/velocity_defconfig "$kroot/.config"
 }
 
 cpc() {
-    cp .config arch/arm64/configs/velocity_defconfig
+    # Don't use savedefconfig for readability and diffability
+    cp "$kroot/.config" arch/arm64/configs/velocity_defconfig
 }
 
 mc() {
@@ -113,23 +123,27 @@ cf() {
 # Because we don't have proper git commit history...
 upstream() {
     curl https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.4.$(($1 - 1))-$1.xz | xz -d | git apply --reject
-    git add .
-    find . -type f -name '*.rej' -exec git reset HEAD {} \;
+    git add "$kroot"
+    find "$kroot" -type f -name '*.rej' -exec git reset HEAD {} \;
 
-    echo -n "$1" > .upstream_ver
+    echo -n "$1" > "$kroot/.upstream_ver"
     echo 'Patch applied. When you are finished fixing conflicts (.rejs) and have verified'
     echo 'that the kernel boots, run upsfinish.'
 }
+
 upsfinish() {
-    find . -type f -name '*.rej' -exec rm -f {} \;
-    git add .
+    find "$kroot" -type f -name '*.rej' -exec rm -f {} \;
+    git add "$kroot"
     git commit -sm "treewide: update to Linux 4.4.$(cat .upstream_ver)"
-    rm -f .upstream_ver
+    rm -f "$kroot/.upstream_ver"
+}
+
+# Edit the raw text config
+ec() {
+    ${EDITOR:-vim} "$kroot/.config"
 }
 
 # Get a sorted list of the side of various objects in the kernel
 osize() {
-    find "$kroot/out" -type f -name '*.o' ! -name 'built-in.o' ! -name 'vmlinux.o' \
-    	-exec du -h --apparent-size {} + | sort -r -h | head -n "${1:-75}" | \
-	perl -pe 's/([\d.]+[A-Z]?).+\/out\/(.+)\.o/$1\t$2.c/g'
+    find "$kroot/out" -type f -name '*.o' ! -name 'built-in.o' ! -name 'vmlinux.o' -exec du -h --apparent-size {} + | sort -r -h | head -n "${1:-75}"
 }
