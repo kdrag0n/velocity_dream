6ec1244c436bfa979276a34f49d49ea66909c9b5
kdrag0n <dragon@khronodragon.com>
2018-12-02 20:26:10 -0800

    helpers: revise osize helper
    Use a regular expression through perl to:
     - Filter out the leading paths up to KBUILD_OUTPUT (out/) so the paths
     given are from the root of the kernel tree to the code
     - Replace .o with .c so the source filenames are given (this generally
     works; the user can figure it out for when it doesn't, such as when the
     Makefile compiles several .c files into a single differently-named .o)
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- helpers.sh
+++ helpers.sh
@@ -129,5 +129,7 @@ upsfinish() {
 
 # Get a sorted list of the side of various objects in the kernel
 osize() {
-    find out -type f -name '*.o' ! -name 'built-in.o' ! -name 'vmlinux.o' -exec du -h --apparent-size {} + | sort -r -h | head -n "${1:-75}"
+    find "$kroot/out" -type f -name '*.o' ! -name 'built-in.o' ! -name 'vmlinux.o' \
+    	-exec du -h --apparent-size {} + | sort -r -h | head -n "${1:-75}" | \
+	perl -pe 's/([\d.]+[A-Z]?).+\/out\/(.+)\.o/$1\t$2.c/g'
 }
