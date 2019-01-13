f95bf275cd435e3632e65a680a587277dc8c12c8
kdrag0n <dragon@khronodragon.com>
2019-01-12 22:36:28 -0800

    timekeeping: keep suspend time logging when debugfs is disabled
    No reason to have thos completely removed when debugfs is off. These
    logs are geniunely useful for debugging.
    
    [  507.944368] Suspended for 1.029 seconds
    [  508.263222] Suspended for 9.972 seconds
    [  508.577067] Suspended for 0.453 seconds
    [  508.870264] Suspended for 4.161 seconds
    [  509.145624] Suspended for 5.714 seconds
    [  509.399533] Suspended for 0.095 seconds
    [  515.153767] Suspended for 4.803 seconds
    [  515.504957] Suspended for 9.939 seconds
    [  515.837413] Suspended for 4.736 seconds
    [  516.181277] Suspended for 9.661 seconds
    [  516.563450] Suspended for 3.283 seconds
    [  516.911800] Suspended for 9.941 seconds
    [  517.261845] Suspended for 9.941 seconds
    [  517.591469] Suspended for 4.740 seconds
    [  517.941479] Suspended for 0.843 seconds
    [  518.299429] Suspended for 8.739 seconds
    [  518.642896] Suspended for 9.947 seconds
    [  518.977214] Suspended for 4.734 seconds
    [  519.323212] Suspended for 9.945 seconds
    [  519.669183] Suspended for 4.722 seconds
    [  520.001467] Suspended for 9.958 seconds
    [  520.348315] Suspended for 3.269 seconds
    [  520.709020] Suspended for 1.091 seconds
    [  521.057833] Suspended for 5.346 seconds
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- kernel/time/timekeeping_internal.h
+++ kernel/time/timekeeping_internal.h
@@ -6,11 +6,7 @@
 #include <linux/clocksource.h>
 #include <linux/time.h>
 
-#ifdef CONFIG_DEBUG_FS
 extern void tk_debug_account_sleep_time(struct timespec64 *t);
-#else
-#define tk_debug_account_sleep_time(x)
-#endif
 
 #ifdef CONFIG_CLOCKSOURCE_VALIDATE_LAST_CYCLE
 static inline cycle_t clocksource_delta(cycle_t now, cycle_t last, cycle_t mask)
